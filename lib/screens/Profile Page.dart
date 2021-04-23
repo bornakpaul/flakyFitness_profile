31df5929a55de453c8f6d46dc6c9cc5cd6120d9a
Bornak <bornakpaul@gmail.com>
2021-04-23 16:53:37 +0530

    Fixed everything to run on iOS.

--- lib/screens/Profile Page.dart
+++ lib/screens/Profile Page.dart
@@ -1,5 +1,4 @@
 import 'dart:ui';
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:profile_flutter_app/screens/GymDescriptionPage.dart';
